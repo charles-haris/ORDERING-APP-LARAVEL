a33dd1a7eb3501fb5c876a8ac864d2f5f3a370a2
BOUASSE BU KOMBILE Charles-Haris <52077878+charles-haris@users.noreply.github.com>
2024-12-22 14:03:41 +0000

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     /**
+    
      * Define the application's command schedule.
      */
     protected function schedule(Schedule $schedule): void
