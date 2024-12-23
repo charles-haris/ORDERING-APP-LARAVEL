ab9aed0d1e22c4a5e85cd1ba09bb7ce220f62cbb
BOUASSE BU KOMBILE Charles-Haris <52077878+charles-haris@users.noreply.github.com>
2024-12-23 00:01:16 +0000

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,7 +19,7 @@ class UserController extends Controller
         $user = User::all();
         return view();
     }
-//this method grants to register
+//this method grants to register user
 
     public function registration(User $user,Request $request){
         $user->first_name = $request->first_name;
